7c0faef234020226b8c14751fe658daaf77234fd
cipher <asaforbrian18@gmail.com>
2023-08-10 03:23:23 +0300

    added some assets and created the bottomNavBar

---
+++ multi_vendor_app/lib/views/buyers/main_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.green,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/explore.svg',
+            ),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/shop.svg'),
+            label: "Store",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/cart.svg'),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/search.svg'),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/account.svg'),
+            label: "Account",
+          ),
+        ],
+      ),
+    );
+  }
+}
