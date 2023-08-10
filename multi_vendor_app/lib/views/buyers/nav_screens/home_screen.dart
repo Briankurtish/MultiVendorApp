ff04ef10dbc9745cd6843815d23399be4722fce3
cipher <asaforbrian18@gmail.com>
2023-08-10 07:02:07 +0300

    created a banner widget on the homescreen

--- multi_vendor_app/lib/views/buyers/nav_screens/home_screen.dart
+++ multi_vendor_app/lib/views/buyers/nav_screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import 'widgets/banner_widget.dart';
 import 'widgets/search_input_widget.dart';
 import 'widgets/welcome_text_widget.dart';
 
@@ -9,18 +10,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
-      child: Column(
-        children: [
-          WelcomeText(),
-          SizedBox(
-            height: 14,
-          ),
-          SearchInputWidget(),
-        ],
-      ),
+    return Column(
+      children: [
+        WelcomeText(),
+        SizedBox(
+          height: 14,
+        ),
+        SearchInputWidget(),
+        BannerWidget()
+      ],
     );
   }
 }
--- multi_vendor_app/lib/views/buyers/nav_screens/widgets/welcome_text_widget.dart
+++ multi_vendor_app/lib/views/buyers/nav_screens/widgets/welcome_text_widget.dart
@@ -8,20 +8,24 @@ class WelcomeText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          "Howdy, What are you\nLooking for? 👀",
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
-        Container(
-          child: SvgPicture.asset(
-            'assets/icons/cart.svg',
-            width: 20,
+    return Padding(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Howdy, What are you\nLooking for? 👀",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
-        ),
-      ],
+          Container(
+            child: SvgPicture.asset(
+              'assets/icons/cart.svg',
+              width: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
