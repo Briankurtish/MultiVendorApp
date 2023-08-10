9c532e66e2119c9869c5cf7a420b86f6f6cad840
cipher <asaforbrian18@gmail.com>
2023-08-10 06:38:06 +0300

    created the welcome text widget and the search widget and added firebase to the project

---
+++ multi_vendor_app/lib/views/buyers/nav_screens/widgets/welcome_text_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "Howdy, What are you\nLooking for? 👀",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          child: SvgPicture.asset(
+            'assets/icons/cart.svg',
+            width: 20,
+          ),
+        ),
+      ],
+    );
+  }
+}
